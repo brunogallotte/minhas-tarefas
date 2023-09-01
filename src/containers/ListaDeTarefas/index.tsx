d557a2101c0af4527230e7fe389e18d6a8e82264
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-09-01 10:27:30 -0300

    Adicionado controle de estado, redux, ação de editar, cancelar, salvar e remover

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux'
+
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import { RootReducer } from '../../store'
+
+const ListaDeTarefas = () => {
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+
+  return (
+    <Container>
+      <p>2 tarefas maracadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+      <ul>
+        {itens.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              id={t.id}
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+
+export default ListaDeTarefas
